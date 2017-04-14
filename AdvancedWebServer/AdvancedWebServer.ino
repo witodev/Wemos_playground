3c460841715cd16f02f0b861197c71af3b34d145
witodev <szwed.witold@gmail.com>
2017-04-14 23:10:29 +0200

    test

--- AdvancedWebServer/AdvancedWebServer.ino
+++ AdvancedWebServer/AdvancedWebServer.ino
@@ -33,8 +33,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char *ssid = "YourSSIDHere";
-const char *password = "YourPSKHere";
+const char *ssid = "YourSSIDHere"; // put your ssid
+const char *password = "YourPSKHere"; // put you password
 
 ESP8266WebServer server(80);
 
